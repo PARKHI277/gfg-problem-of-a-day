0096fcb74b4dffd106f519929843cb7cd0864e79
Parkhi <parkhigarg277@gmail.com>
2022-06-14 15:06:29 +0530

    helptheif

---
+++ helpatheif.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<queue>
+#include<bits/stdc++.h>
+int maxCoins(int A[], int B[], int T, int N) {
+        // code here
+        if(T== 0 || N==0)
+        {
+            return 0;
+        }
+        int ans = 0; // max coin
+        
+        priority_queue<pair<int,int>>pq;
+        
+        for(int i = 0;i<N;i++)
+        {
+            int coins = B[i];
+            int plate = A[i];
+            pq.push({coins,plate});
+        }
+        
+        while(T != 0)
+        {
+            int no_of_coins = pq.top().first;
+            int total_plates = pq.top().second;
+            pq.pop();
+            
+            for(int i = 0;i<total_plates;i++)
+            {
+                if(T == 0)
+                 break;
+                 ans = ans + no_of_coins;
+                 T--;
+            }
+            
+            if(pq.empty())
+              break;
+        }
+        
+        return ans;
+    }
